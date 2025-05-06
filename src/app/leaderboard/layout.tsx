e89e54b729bf06b202103678f25e5124c07f2121
James.Y <cyang4@macalester.edu>
2025-05-06 03:43:59 -0500

    feat: added sidebar and routes in leaderboard layout

--- src/app/leaderboard/layout.tsx
+++ src/app/leaderboard/layout.tsx
@@ -1,14 +1,29 @@
 import TopNavBar from "@/components/TopNavBar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { cookies } from "next/headers";
 
 export default async function LeaderboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+  
   return (
-    <div>
-      <TopNavBar />
-      {children}
+    <div className="flex flex-col">
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar />
+        <main className="w-full flex flex-col">
+          <div className="flex flex-row items-center px-4 border-b">
+            <SidebarTrigger />
+            <TopNavBar />
+          </div>
+
+          {children}
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
