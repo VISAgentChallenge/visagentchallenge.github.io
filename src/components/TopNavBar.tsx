232e333f7825877450a0c39efc7c6321a3fc0491
James.Y <cyang4@macalester.edu>
2025-05-07 02:40:53 -0500

    patch: updated top nav bar for landing page (added challenge and team)

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,6 +1,6 @@
 import { Button } from "./ui/button";
 import { signOut } from "@/auth";
-import { LogOut } from "lucide-react";
+import { LogOut, LogIn, UsersRound, Milestone } from "lucide-react";
 
 export default function TopNavBar() {
   const handleLogout = async () => {
@@ -9,14 +9,27 @@ export default function TopNavBar() {
   };
 
   return (
-    <nav className="p-4 w-full">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold"></div>
+    <nav className="p-4 w-full border-b">
+      <div className="container max-w-5xl mx-auto flex justify-between items-center">
+        <div className="text-lg font-bold">VIS Workshop</div>
         <div className="flex space-x-4">
           {/* add theme provider */}
-          <Button variant="ghost" size="icon" onClick={handleLogout}>
-            <LogOut className="h-5 w-5" />
+          <Button variant={"ghost"} >
+            <Milestone className="h-5 w-5" />
+            <span>Challenge</span>
           </Button>
+          <Button variant={"ghost"} >
+            <UsersRound className="h-5 w-5" />
+            <span>Team</span>
+          </Button>
+          <Button variant="ghost" >
+            <LogIn className="h-5 w-5" />
+            <span>Login</span>
+          </Button>
+          {/* <Button variant="ghost" onClick={handleLogout}>
+            <LogOut className="h-5 w-5" />
+            <span>Logout</span>
+          </Button> */}
         </div>
       </div>
     </nav>
