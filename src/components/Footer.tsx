e956ddde3d1495d37fa0023a01504af85b853c77
James.Y <cyang4@macalester.edu>
2025-05-07 02:41:23 -0500

    feat: added footer for landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+export default function Footer() {
+  return (
+    <nav className="p-4 w-full border-t">
+      <div className="container max-w-5xl mx-auto flex justify-between items-center">
+        {`Footer content goes here`}
+      </div>
+    </nav>
+  );
+}
