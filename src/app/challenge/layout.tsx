18c5a30664611abef4c296481540fab0ef0af63d
James.Y <cyang4@macalester.edu>
2025-05-09 04:54:34 -0500

    feat: created empty '/challenge' route

---
+++ src/app/challenge/layout.tsx
@@ -0,0 +1,17 @@
+import TopNavBar from "@/components/TopNavBar/TopNavBar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { cookies } from "next/headers";
+
+export default function ChallengeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col">
+      <div>{`challenge layout`}</div>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/challenge/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Challenge() {
+  return (
+    <div className="p-12 flex flex-col gap-4">
+      <div>{`challenge page`}</div>
+    </div>
+  );
+}
