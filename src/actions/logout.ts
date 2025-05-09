528393dcee57e594ef023e07dd44f4be17e60ab6
James.Y <cyang4@macalester.edu>
2025-05-09 04:54:10 -0500

    feat: created logout action

---
+++ src/actions/logout.ts
@@ -0,0 +1,11 @@
+"use server";
+import { auth, signIn, signOut } from "@/auth";
+
+export async function logout() {
+  try {
+    console.log("Logging out...");
+    await signOut({ redirectTo: "/" });
+  } catch (error) {
+    return error;
+  }
+}
