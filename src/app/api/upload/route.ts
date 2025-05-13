bf178f260ea0b116e2af7524e2badd128dd819b5
James.Y <cyang4@macalester.edu>
2025-05-13 05:38:07 -0500

    feat: drafted upload api route (to do)

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { mkdir, writeFile } from "fs/promises";
+import path from "path";
+
+export async function POST(req: NextRequest) {
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
+
+  if (!file || file.type !== "application/zip") {
+    return NextResponse.json({ error: "Invalid file" }, { status: 400 });
+  }
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  // update here with server endpoint
+  //   const uploadDir = path.join(process.cwd(), 'public', 'uploads');
+  //   await mkdir(uploadDir, { recursive: true });
+
+  //   const filePath = path.join(uploadDir, file.name);
+  //   await writeFile(filePath, buffer);
+
+  return NextResponse.json({ message: "File uploaded" });
+}
