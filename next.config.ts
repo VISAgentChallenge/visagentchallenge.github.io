9688354be7e673f6390607df56efb78bb3338a39
James.Y <cyang4@macalester.edu>
2025-05-16 10:32:41 -0500

    fix: CORS issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/leaderboard",
+        destination: "http://52.250.67.65:8000/leaderboard",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
