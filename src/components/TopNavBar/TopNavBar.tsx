5f89bd09b5109c0971584bb3aa2f7e034e64792e
James.Y <cyang4@macalester.edu>
2025-05-30 03:41:25 -0500

    fix: top nav bar transparency & some syntax aligned with tailwindCSS

--- src/components/TopNavBar/TopNavBar.tsx
+++ src/components/TopNavBar/TopNavBar.tsx
@@ -21,11 +21,8 @@ export default function TopNavBar({ session }: { session: Session | null }) {
   };
 
   return (
-
-    <nav className="fixed top-0 w-full z-50 py-5.5 px-10 font-sans text-lg"
-      style={{
-        backgroundColor: `rgba(218, 234, 245, 0.5)`,
-      }}
+    <nav
+      className="fixed top-0 w-full z-50 py-5.5 px-10 font-sans text-lg bg-[#ecf4fa]"
     >
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between font-inter">
         {/* Title / Home link */}
@@ -38,48 +35,55 @@ export default function TopNavBar({ session }: { session: Session | null }) {
 
         {/* Nav buttons */}
         <div className="flex items-center space-x-1">
-          <Button variant="ghost" onClick={() => redirect("/")}
+          <Button
+            variant="ghost"
+            onClick={() => redirect("/")}
             className="px-3 py-2 rounded-md text-base transition-colors text-[#333] hover:bg-[#ACD2ED] font-normal"
           >
-            <Home className="h-5 w-5"/>
+            <Home className="h-5 w-5" />
             <span>Overview</span>
           </Button>
-          <Button variant={"ghost"} onClick={() => redirect("/challenge")}
+          <Button
+            variant={"ghost"}
+            onClick={() => redirect("/challenge")}
             className="px-3 py-2 rounded-md text-base transition-colors text-[#333] hover:bg-[#ACD2ED] font-normal"
           >
-
             <Milestone className="h-5 w-5" />
             <span>Challenge</span>
           </Button>
-          <Button variant="ghost" onClick={() => redirect("/guides")}
+          <Button
+            variant="ghost"
+            onClick={() => redirect("/guides")}
             className="px-3 py-2 rounded-md text-base transition-colors text-[#333] hover:bg-[#ACD2ED] font-normal"
           >
             <Book className="h-5 w-5" />
             <span>Guides</span>
           </Button>
           {session?.user && (
-
-            <Button variant="ghost" onClick={() => redirect("/submission")}
+            <Button
+              variant="ghost"
+              onClick={() => redirect("/submission")}
               className="px-3 py-2 rounded-md text-base transition-colors text-[#333] hover:bg-[#ACD2ED] font-normal"
             >
-
               <Layers2 className="h-5 w-5" />
               <span>Submissions</span>
             </Button>
           )}
           {session?.user ? (
-            <Button variant="ghost" onClick={handleLogout}
+            <Button
+              variant="ghost"
+              onClick={handleLogout}
               className="px-3 py-2 rounded-md text-base transition-colors text-[#333] hover:bg-[#ACD2ED] font-normal"
             >
               <LogOut className="h-5 w-5" />
               <span>Logout</span>
             </Button>
           ) : (
-
-            <Button variant="ghost" onClick={() => redirect("/signin")}
+            <Button
+              variant="ghost"
+              onClick={() => redirect("/signin")}
               className="px-3 py-2 rounded-md text-base transition-colors text-[#333] hover:bg-[#ACD2ED] font-normal"
             >
-
               <LogIn className="h-5 w-5" />
               <span>Login</span>
             </Button>
